e8b1f1caa65dddb4f01c5e4c0bb5a4c60b8bdac9
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-07-01 20:47:39 +0100

    socket.io connect/disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,19 +1,36 @@
 const express = require('express');
-const app = express();
+const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
 
 const port = process.env.PORT || 5050;
 const publicPath = path.join(__dirname, '../public');
-
-app.use(express.static(publicPath));
-
-
 // publicPath. the path to the dir is cleaner and cross-os friendly
 // console.log(__dirname + '/../public');
 // C:\Users\Hitesh\Desktop\Code\webDev\Back-end\NodeJs\Udemy-course\socketIO\node-chat-app\server/../public
-console.log(publicPath);
+// console.log(publicPath);
 // C:\Users\Hitesh\Desktop\Code\webDev\Back-end\NodeJs\Udemy-course\socketIO\node-chat-app\public
 
-app.listen(port, () => {
+const app = express();
+const server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log(`new user connected`);
+
+  socket.on('disconnect', () => {
+    console.log(`user disconnected`);
+  });
+});
+
+
+
+
+server.listen(port, () => {
   console.log(`\nRunning Express - port:${port}\n`);
 });
+
+// socket.io.js available at
+// localhost:5050/socket.io/socket.io.js
