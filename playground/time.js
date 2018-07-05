a847c668667e067b7e37bd9734a0882d8a91d267
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-07-05 19:43:41 +0100

    time

---
+++ playground/time.js
@@ -0,0 +1,18 @@
+console.log('\n');
+// UNIX epoch time - needs to be converted
+// started: Jan 1st 1970 00:00:00am UTC
+
+const moment = require('moment');
+
+// const date = new Date();
+// console.log(date.getMonth());
+
+var date = moment();
+
+// console.log(date.format('MMM Do, YYYY'));
+
+console.log(date.format('HH:MM a \n'));
+
+
+morn = date.subtract(12, 'hours').format('H:MM a')
+console.log(morn);
